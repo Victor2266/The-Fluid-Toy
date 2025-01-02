78ec070f5da950321a7b9e4862fe5e85142cd705
JJ Marr <jjmarr@gmail.com>
2025-01-01 22:23:05 -0500

    Change hash algorithm
    This is 1-to-1

--- Fluid Simulation/Assets/Scripts/Sim 2D/Compute/SpatialHash.hlsl
+++ Fluid Simulation/Assets/Scripts/Sim 2D/Compute/SpatialHash.hlsl
@@ -11,10 +11,6 @@ static const int2 offsets2D[9] =
 	int2(1, -1),
 };
 
-// Constants used for hashing
-static const uint hashK1 = 15823;
-static const uint hashK2 = 9737333;
-
 // Convert floating point position into an integer cell coordinate
 int2 GetCell2D(float2 position, float radius)
 {
@@ -24,10 +20,7 @@ int2 GetCell2D(float2 position, float radius)
 // Hash cell coordinate to a single unsigned integer
 uint HashCell2D(int2 cell)
 {
-	cell = (uint2)cell;
-	uint a = cell.x * hashK1;
-	uint b = cell.y * hashK2;
-	return (a + b);
+	return (cell.x << 16) | (cell.y + (2 << 15 - 1));
 }
 
 uint KeyFromHash(uint hash, uint tableSize)
