01a2e4dcf9162fc608c0fad1c75f8428a46ae445
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2025-01-12 20:58:27 -0500

    Update Simulation2DAoS_countingSort.cs

--- Fluid Simulation/Assets/Scripts/Sim 2D/Simulation2DAoS_countingSort.cs
+++ Fluid Simulation/Assets/Scripts/Sim 2D/Simulation2DAoS_countingSort.cs
@@ -5,44 +5,6 @@ using System;
 using UnityEngine.UIElements;
 using System.Linq;
 
-//Defining Structs
-/*[System.Serializable]
-[StructLayout(LayoutKind.Sequential, Size = 40)]
-public struct Particle // 40 bytes total
-{
-    public float2 density; //8 bytes, density and near density
-    public Vector2 velocity; //8 bytes
-    public Vector2 predictedPosition; // 8
-    public Vector2 position; // 8
-    public float temperature; // 4
-    public FluidType type; // 4 (enum is int by default)
-}
-
-[System.Serializable]
-[StructLayout(LayoutKind.Sequential, Size = 12)]
-public struct Circle //12 bytes total
-{
-    public Vector2 pos; //8 bytes
-    public float radius; //4 bytes
-}
-
-[System.Serializable]
-[StructLayout(LayoutKind.Sequential, Size = 16)]
-public struct SourceObject //16 bytes total
-{
-    public Vector2 pos; //8 bytes
-    public float radius; //4 bytes
-    public int fluidType; //4 bytes
-}
-
-[System.Serializable]
-[StructLayout(LayoutKind.Sequential, Size = 24)]
-public struct OrientedBox //24 bytes total
-{
-    public Vector2 pos; //8 bytes
-    public Vector2 size;
-    public Vector2 zLocal;
-};*/
 
 public class Simulation2DAoSCounting : MonoBehaviour, IFluidSimulation
 {
