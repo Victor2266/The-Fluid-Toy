3f05f6f27b43cb316a614faa6bdb63719d6cd30c
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2025-01-12 21:08:50 -0500

    Update Simulation2DAoS_countingSort.cs

--- Fluid Simulation/Assets/Scripts/Sim 2D/Simulation2DAoS_countingSort.cs
+++ Fluid Simulation/Assets/Scripts/Sim 2D/Simulation2DAoS_countingSort.cs
@@ -124,7 +124,7 @@ public class Simulation2DAoSCounting : MonoBehaviour, IFluidSimulation
     private float accumulatedTime = 0f;
     private const float MAX_DELTA_TIME = 1f/30f; // Maximum allowed delta time
     private const float FIXED_TIME_STEP = 1f/120f; // Your desired fixed time step
-
+    
     void Start()
     {
         Debug.Log("Controls: Space = Play/Pause, R = Reset, LMB = Attract, RMB = Repel");
@@ -204,7 +204,7 @@ public class Simulation2DAoSCounting : MonoBehaviour, IFluidSimulation
         spatialIndices = ComputeHelper.CreateStructuredBuffer<uint3>(numParticles);
         spatialOffsets = ComputeHelper.CreateStructuredBuffer<uint>(numParticles);
         sortedIndices = ComputeHelper.CreateStructuredBuffer<uint>(numParticles);
-        
+
         // Set buffer data
         fluidDataBuffer.SetData(fluidParamArr);
         ScalingFactorsBuffer.SetData(scalingFactorsArr);
