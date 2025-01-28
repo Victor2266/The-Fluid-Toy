38159df8fd779d3ae23e112b14153f8e8ea50b42
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2025-01-27 21:57:06 -0500

    added animation to tooltip

--- Fluid Simulation/Assets/Scripts/UI/TooltipManager.cs
+++ Fluid Simulation/Assets/Scripts/UI/TooltipManager.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using DG.Tweening;
 using UnityEngine;
 
 public class TooltipManager : MonoBehaviour
@@ -7,7 +8,11 @@ public class TooltipManager : MonoBehaviour
     
     [SerializeField] private RectTransform tooltipPanel;
     [SerializeField] private TextMeshProUGUI tooltipText;
-    
+    [SerializeField] private float fadeTime = 0.2f;
+    private CanvasGroup canvasGroup;
+    private Sequence currentSequence;
+
+
     private void Awake()
     {
         if (Instance == null)
@@ -20,15 +25,33 @@ public class TooltipManager : MonoBehaviour
             Instance = this;
         }
     }
+    private void Start()
+    {
+        canvasGroup = tooltipPanel.GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+        tooltipPanel.gameObject.SetActive(false);
+
+    }
 
     public void ShowTooltip(string message)
     {
+        // Kill any ongoing animations
+        currentSequence?.Kill();
+        canvasGroup.DOKill();
+
         tooltipPanel.gameObject.SetActive(true);
         tooltipText.text = message;
+        currentSequence = DOTween.Sequence().Append(canvasGroup.DOFade(1, fadeTime).SetEase(Ease.OutCubic));
     }
 
     public void HideTooltip()
     {
-        tooltipPanel.gameObject.SetActive(false);
+        // Kill any ongoing animations
+        currentSequence?.Kill();
+        canvasGroup.DOKill();
+
+        currentSequence = DOTween.Sequence()
+            .Append(canvasGroup.DOFade(0, fadeTime).SetEase(Ease.OutCubic))
+            .OnComplete(() => tooltipPanel.gameObject.SetActive(false));
     }
 }
