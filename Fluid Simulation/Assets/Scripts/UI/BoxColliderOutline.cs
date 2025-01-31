baaace327da0a0aa17861d106237ca2644b84bec
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2025-01-31 00:25:05 -0500

    replaced the box outline from sprites to line render for better scaling support

---
+++ Fluid Simulation/Assets/Scripts/UI/BoxColliderOutline.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[ExecuteInEditMode] // This makes it run in editor
+public class BoxColliderOutline : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private BoxCollider2D boxCollider;
+    
+    void Start()
+    {
+        // Setup Line Renderer
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 4; // 4 points to close the rectangle
+        
+        // Get collider bounds
+        boxCollider = GetComponent<BoxCollider2D>();
+        UpdateOutline();
+    }
+    
+    void UpdateOutline()
+    {
+        Vector2 size = boxCollider.size;
+        Vector2 offset = boxCollider.offset;
+        
+        // Calculate corners
+        Vector3[] positions = new Vector3[4];
+        positions[0] = transform.TransformPoint(new Vector3(-size.x/2 + offset.x, -size.y/2 + offset.y, 0));
+        positions[1] = transform.TransformPoint(new Vector3(size.x/2 + offset.x, -size.y/2 + offset.y, 0));
+        positions[2] = transform.TransformPoint(new Vector3(size.x/2 + offset.x, size.y/2 + offset.y, 0));
+        positions[3] = transform.TransformPoint(new Vector3(-size.x/2 + offset.x, size.y/2 + offset.y, 0));
+        
+        lineRenderer.SetPositions(positions);
+    }
+
+
+
+    void Update()
+    {
+        UpdateOutline();
+    }
+
+
+}
