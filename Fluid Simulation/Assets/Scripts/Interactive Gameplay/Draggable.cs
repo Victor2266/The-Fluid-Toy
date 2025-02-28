fb867de07faa60b385608ef32f3e7aa29768277b
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2025-02-27 21:47:28 -0500

    Update Draggable.cs

--- Fluid Simulation/Assets/Scripts/Interactive Gameplay/Draggable.cs
+++ Fluid Simulation/Assets/Scripts/Interactive Gameplay/Draggable.cs
@@ -40,7 +40,7 @@ public class Draggable : MonoBehaviour
     {
         isDragging = false;
         if (rb2d != null) {
-            rb2d.bodyType = RigidbodyType2D.Kinematic;
+            rb2d.bodyType = RigidbodyType2D.Dynamic;
             rb2d.freezeRotation = false;
         }
     }
