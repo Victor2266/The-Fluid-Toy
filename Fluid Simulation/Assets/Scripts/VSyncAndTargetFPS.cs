b46e2200925ce2f0eb03731e34ec8d59599d623b
Cameron Tuffner-Lyons <friitztm@gmail.com>
2025-03-08 12:55:41 -0500

    fix

--- Fluid Simulation/Assets/Scripts/VSyncAndTargetFPS.cs
+++ Fluid Simulation/Assets/Scripts/VSyncAndTargetFPS.cs
@@ -24,18 +24,11 @@ public class VSyncAndTargetFPS : MonoBehaviour
         //          Android and iOS: Content is rendered at fixed 30 fps to conserve battery power, independent of the native refresh rate of the display.
 
         QualitySettings.vSyncCount = vsyncCount; // Set vSyncCount to 0 so that using .targetFrameRate is enabled.
-    }
-
-    void Update()
-    {
-        if(!updateFrameRate){
-            if(PlayerPrefs.GetInt($"FPS_UNCAP", 0) == 1){
-                Application.targetFrameRate = -1;
-            }else{
-                Application.targetFrameRate = (int)Screen.currentResolution.refreshRateRatio.value;
-            }
-            updateFrameRate = true;
+        if(PlayerPrefs.GetInt($"FPS_UNCAP", 0) == 1){
+            Application.targetFrameRate = -1;
+        }else{
+            Application.targetFrameRate = (int)Screen.currentResolution.refreshRateRatio.value;
         }
-        
+        updateFrameRate = true;
     }
 }
