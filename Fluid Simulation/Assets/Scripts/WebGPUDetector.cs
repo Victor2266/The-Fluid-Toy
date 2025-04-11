44160e1e610d47850e3bbddcdb3781e35fb222f0
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2025-04-11 00:26:45 -0400

    added fallback for no webGPU detected

---
+++ Fluid Simulation/Assets/Scripts/WebGPUDetector.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WebGPUDetector : MonoBehaviour
+{
+    [SerializeField] private string webGPUFallbackSceneName = "WebGPUInstructions";
+    [SerializeField] private bool debugMode = true;
+    
+    private void Start()
+    {
+        if (debugMode)
+        {
+            Debug.Log("WebGPUDetector started");
+            Debug.Log("Current graphics device: " + SystemInfo.graphicsDeviceType);
+        }
+        
+        // Check if we're using WebGPU
+        bool usingWebGPU = IsUsingWebGPU();
+        
+        if (debugMode)
+        {
+            Debug.Log("Using WebGPU: " + usingWebGPU);
+        }
+        
+        // If we're not using WebGPU, switch to instructions scene
+        if (!usingWebGPU)
+        {
+            if (debugMode)
+            {
+                Debug.Log("WebGPU not detected, loading instructions scene: " + webGPUFallbackSceneName);
+            }
+            
+            // Make sure the scene is included in build settings
+            if (SceneUtility.GetBuildIndexByScenePath(webGPUFallbackSceneName) != -1)
+            {
+                SceneManager.LoadSceneAsync(webGPUFallbackSceneName);
+            }
+            else
+            {
+                Debug.LogError("Scene not found in build settings: " + webGPUFallbackSceneName);
+            }
+        }
+    }
+    
+    private bool IsUsingWebGPU()
+    {
+        // Check the graphics device type directly
+        // WebGPU will be reported as "WebGPU" in SystemInfo.graphicsDeviceType
+        return SystemInfo.graphicsDeviceType.ToString() == "WebGPU";
+    }
+}
