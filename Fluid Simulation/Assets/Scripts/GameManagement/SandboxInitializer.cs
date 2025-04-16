89addfa12e6d08aa164f2bf336f4918b5461184e
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2025-04-16 16:15:42 -0400

    fixing obstacle scaling for sandbox

--- Fluid Simulation/Assets/Scripts/GameManagement/SandboxInitializer.cs
+++ Fluid Simulation/Assets/Scripts/GameManagement/SandboxInitializer.cs
@@ -9,12 +9,12 @@ public class SandboxInitializer : MonoBehaviour
     private float[] cameraSizes = { 5.03f, 7.05f, 9.87f, 13.82f, 19.35f};
     public new Camera camera;
 
-    private Vector2[] simulationBounds = {new Vector2(17.81f, 10f), new Vector2(24.93f, 14f), new Vector2(34.9f, 19.6f), new Vector2(48.86f, 27.44f), new Vector2(68.40f, 38.42f)};
+    // private Vector2[] simulationBounds = {new Vector2(17.81f, 10f), new Vector2(24.93f, 14f), new Vector2(34.9f, 19.6f), new Vector2(48.86f, 27.44f), new Vector2(68.40f, 38.42f)};
     private int[] simulationMaxParticles = {4000, 8000, 16000, 32000, 64000};
     public IFluidSimulation sim;
 
-    private float[] topObstacleBoundaryPositions = {12.5f, 14.5f, 17.3f, 21.22f, 26.71f};
-    private float[] sideObstacleBoundaryPositions = {16.41f, 19.965f, 24.95f, 31.93f, 41.7f};
+    // private float[] topObstacleBoundaryPositions = {12.5f, 14.5f, 17.3f, 21.22f, 26.71f};
+    // private float[] sideObstacleBoundaryPositions = {16.41f, 19.965f, 24.95f, 31.93f, 41.7f};
 
     public GameObject[] obstacleBoundarys; // Top, Bottom, Left, Right
 
@@ -27,33 +27,30 @@ public class SandboxInitializer : MonoBehaviour
 
         // Set up camera
         camera.orthographicSize = cameraSizes[presetIndex];
+        float boundsWidth = camera.orthographicSize * 2f * Screen.width / Screen.height;
+        float boundsHeight = camera.orthographicSize * 2f;
 
         // Set up Obstacle Boundary colliders positions
-        obstacleBoundarys[0].transform.localPosition = new Vector3(0, topObstacleBoundaryPositions[presetIndex], 0);
-        obstacleBoundarys[1].transform.localPosition = new Vector3(0, -topObstacleBoundaryPositions[presetIndex], 0);
-        obstacleBoundarys[2].transform.localPosition = new Vector3(-sideObstacleBoundaryPositions[presetIndex], 0, 0);
-        obstacleBoundarys[3].transform.localPosition = new Vector3(sideObstacleBoundaryPositions[presetIndex], 0, 0);
+        obstacleBoundarys[0].transform.localPosition = new Vector3(0, (boundsHeight + 15f)/2f, 0);
+        obstacleBoundarys[1].transform.localPosition = new Vector3(0, -(boundsHeight + 15f)/2f, 0);
+        obstacleBoundarys[2].transform.localPosition = new Vector3(-(boundsWidth + 15f)/2f, 0, 0);
+        obstacleBoundarys[3].transform.localPosition = new Vector3((boundsWidth + 15f)/2f, 0, 0);
 
         // Set up Obstacle Boundary colliders sizes
-        obstacleBoundarys[0].GetComponent<BoxCollider2D>().size = new Vector2(topObstacleBoundaryPositions[presetIndex] * 3.5f, 15f);
-        obstacleBoundarys[1].GetComponent<BoxCollider2D>().size = new Vector2(topObstacleBoundaryPositions[presetIndex] * 3.5f, 15f);
-        obstacleBoundarys[2].GetComponent<BoxCollider2D>().size = new Vector2(15f, sideObstacleBoundaryPositions[presetIndex] * 2f);
-        obstacleBoundarys[3].GetComponent<BoxCollider2D>().size = new Vector2(15f, sideObstacleBoundaryPositions[presetIndex] * 2f);
+        obstacleBoundarys[0].GetComponent<BoxCollider2D>().size = new Vector2((boundsHeight + 15f)/2f * 3.5f, 15f);
+        obstacleBoundarys[1].GetComponent<BoxCollider2D>().size = new Vector2((boundsHeight + 15f)/2f * 3.5f, 15f);
+        obstacleBoundarys[2].GetComponent<BoxCollider2D>().size = new Vector2(15f, (boundsWidth + 15f)/2f * 2f);
+        obstacleBoundarys[3].GetComponent<BoxCollider2D>().size = new Vector2(15f, (boundsWidth + 15f)/2f * 2f);
 
         // Find the fluid simulation in the scene
         GameObject simulationGameobject = GameObject.FindGameObjectWithTag("Simulation");
         sim = simulationGameobject.GetComponent<IFluidSimulation>();
 
         // Update simulation settings
-        sim.setBounds(simulationBounds[presetIndex]);
+        sim.setBounds(new Vector2(boundsWidth, boundsHeight));   
         sim.setMaxParticles(simulationMaxParticles[presetIndex]);
 
         // Destroy the sandbox initializer on complete
         //Destroy(gameObject);
-    }
-
-    void Update()
-    {
-        sim.setBounds(new Vector2(camera.orthographicSize * 2f * Screen.width / Screen.height, camera.orthographicSize * 2f));   
     }
 }
