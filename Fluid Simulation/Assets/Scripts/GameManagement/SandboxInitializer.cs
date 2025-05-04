a488411ea48012c41875cddd23c64971ec605e51
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2025-05-04 00:03:29 -0400

    added 128k-2M particle options in hidden menu

--- Fluid Simulation/Assets/Scripts/GameManagement/SandboxInitializer.cs
+++ Fluid Simulation/Assets/Scripts/GameManagement/SandboxInitializer.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class SandboxInitializer : MonoBehaviour
 {
-    private string[] sandboxSubtitles = {"4k", "8k", "16k", "32k", "64k"};
+    private string[] sandboxSubtitles = {"4k", "8k", "16k", "32k", "64k", "128k", "256k", "512k", "1.024M", "2.048M",};
     public TMP_Text sandboxTitleText;
-    private float[] cameraSizes = { 5.03f, 7.05f, 9.87f, 13.82f, 19.35f};
+    private float[] cameraSizes = { 5.03f, 7.05f, 9.87f, 13.82f, 19.35f, 19.35f, 27.3f, 38.5f, 54.4f, 76.5f};
     public Camera sceneCamera;
-    private int[] simulationMaxParticles = {4000, 8000, 16000, 32000, 64000};
+    private int[] simulationMaxParticles = {4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000, 1024000, 2048000};
     public IFluidSimulation sim;
 
 
--- Fluid Simulation/Assets/Scripts/UI/SandboxSetupManager.cs
+++ Fluid Simulation/Assets/Scripts/UI/SandboxSetupManager.cs
@@ -11,26 +11,82 @@ public class SandboxSetupManager : MonoBehaviour
 
     public Slider sandboxSettingSlider;
     public TMP_Text sandboxSettingText;
+    private string[] sandboxSettings = { "(4k)", "Low (8k)", "Medium (16k)", "High (32k)", "(64k)", "(128k)", "(256k)", "(512k)", "(1.024M)", "(2.048M)" }; //{"Very Low (4k)", "Low (8k)", "Medium (16k)", "High (32k)", "Ultra High (64k)"};
 
-    private string[] sandboxSettings = {"(4k)", "Low (8k)", "Medium (16k)", "High (32k)", "(64k)"}; //{"Very Low (4k)", "Low (8k)", "Medium (16k)", "High (32k)", "Ultra High (64k)"};
+
+    // Higher Particle Count Settings
+    private bool highParticleCountOptions = false;
+    public AudioSource audioSource;
+    public AudioClip showHiddenMenuSound;
+    public TMP_Text subtitle1Text;
+    private string subtitle1original;
+    private string subtitle1alt = "<color=red>WARNING!";
+    public TMP_Text subtitle2Text;
+    private string subtitle2original;
+    private string subtitle2alt = "\nTHESE SETTINGS CAN ABSOLUTELY OBLITERATE YOUR COMPUTER!\nPress [G] to return to lower settings";
+
 
     private void Start()
     {
+        subtitle1original = subtitle1Text.text;
+        subtitle2original = subtitle2Text.text;
         Initialize();
     }
 
-    public void Initialize(){
+    public void Initialize()
+    {
         // Load sandbox settings and apply to the slider
         int savedSandboxPreset = PlayerPrefs.GetInt("SandboxPreset", 2);
-        sandboxSettingSlider.value = savedSandboxPreset;
+        if (savedSandboxPreset >= 5)
+        {
+            ToggleHighParticleCounts();
+            sandboxSettingSlider.value = savedSandboxPreset - 5;
+        }
+        else
+        {
+            highParticleCountOptions = false;
+            sandboxSettingSlider.value = savedSandboxPreset;
+        }
+
         sandboxSettingText.text = sandboxSettings[savedSandboxPreset];
         sandboxSettingSlider.onValueChanged.AddListener(OnSandBoxPresetChanged);
     }
 
     private void OnSandBoxPresetChanged(float presetIndex)
     {
-        PlayerPrefs.SetInt("SandboxPreset", Mathf.RoundToInt(presetIndex));
-        sandboxSettingText.text = sandboxSettings[Mathf.RoundToInt(presetIndex)];
+        int presetIndexInt = Mathf.RoundToInt(presetIndex);
+        if (highParticleCountOptions)
+        {
+            presetIndexInt += 5;
+        }
+        PlayerPrefs.SetInt("SandboxPreset", presetIndexInt);
+        sandboxSettingText.text = sandboxSettings[presetIndexInt];
         PlayerPrefs.Save();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ToggleHighParticleCounts();
+        }
+    }
+
+    void ToggleHighParticleCounts()
+    {
+        audioSource.PlayOneShot(showHiddenMenuSound, 0.5f);
+        if (highParticleCountOptions)
+        {
+            subtitle1Text.text = subtitle1original;
+            subtitle2Text.text = subtitle2original;
+            highParticleCountOptions = false;
+        }
+        else
+        {
+            subtitle1Text.text = subtitle1alt;
+            subtitle2Text.text = subtitle2alt;
+            highParticleCountOptions = true;
+        }
+        OnSandBoxPresetChanged(sandboxSettingSlider.value);
     }
 }
